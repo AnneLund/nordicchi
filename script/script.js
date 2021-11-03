3b690824ad00465861a5b79a6aa01c653a3aca5d
AnneLund <89774650+AnneLund@users.noreply.github.com>
2021-11-03 08:16:32 +0100

    ...

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", nav);
 function nav() {
   const burger = document.querySelector(".burger");
-  const nav = document.querySelector("nav");
+  const nav = document.querySelector(".main-nav");
   burger.addEventListener("click", () => {
     nav.classList.toggle("show");
   });
